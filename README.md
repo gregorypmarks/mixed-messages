d335e5aea24a6fabaac1256c4c9bd138477c4cf6
gregorypmarks <36426526+gregorypmarks@users.noreply.github.com>
2021-03-15 14:31:58 +0000

    Project displays a random quote for opening

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# mixed-messages
+Mixed Messages App
+================
+It is the first [Code Academy](https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages) portfolio project.
+
+Introduction
+------------
+The app will be a simple console application that will generate a random quote for every opening.
+
+Technologies
+-------------
+* NodeJS
+* JavaScript
+
+Launch
+-------
+To run the application run the following command in the directory where is your file located:
+```javascript
+node mixed-messages.js
+```
---
+++ mixed-messages.js
@@ -0,0 +1,19 @@
+// Quote array that contains wise quotes
+const quotes = [
+    "Marcus Aurelius: Very little is needed to make a happy life; it is all within yourself, in your way of thinking.", 
+    "Einstein: If you can't explain it simply, you don't understand it well enough.", "Steve Jobs: I'm as proud of what we don't do as I am of what we do.", 
+    "Nietzsche: That which does not kill us makes us stronger.", 
+    "Plato: Wise men talk because they have something to say; fools, because they have to say something.", 
+    "Socrates: An unexamined life is not worth living.", 
+    "Cicero: Any man can make mistakes, but only an idiot persists in his error."
+];
+
+// Random element selected from the array
+const randomQuote = (array) => Math.floor(Math.random() * array.length);
+
+// Selects the quote
+const selectedQuote = quotes[randomQuote(quotes)];
+
+// Dispalys the Quote
+console.log('Your quote for the day:')
+console.log(selectedQuote);
